797fbdb3b59ca0d92d35d87622ccf4a0f375ea8a
rachpt <rachpt@126.com>
2018-12-31 19:52:48 +0800

    Update ip_address.sh

--- ip_address.sh
+++ ip_address.sh
@@ -4,7 +4,7 @@
 if [[ $# -ge 1 ]]; then
   data=$(http GET 'http://ip.lockview.cn/default.aspx')
   pam1="$(echo "$data"|grep 'id="__VIEWSTATE"'|head -1|awk -F 'value="' '{print $2}'|sed 's!" />.*!!g')"
-  pam2="$(echo "$data"|grep '__VIEWSTATEGENERATOR'|awk -F 'value="' '{print $2}'|grep -Eo '[a-zA-Z0-9]{8}|'head -1)"
+  pam2="$(echo "$data"|grep '__VIEWSTATEGENERATOR'|awk -F 'value="' '{print $2}'|grep -Eo '[a-zA-Z0-9]{8}'|head -1)"
   ipaddr="$(echo "$1"|grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
   sleep 1
   http -f POST 'http://ip.lockview.cn/default.aspx' txtSearchLine="$ipaddr" \
