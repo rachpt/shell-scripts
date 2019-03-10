11117a08ad13d561c16921f22ec7abb39ce12cc2
rachpt <rachpt@126.com>
2019-03-10 12:08:26 +0800

    fix wrong equation

--- elec.sh
+++ elec.sh
@@ -12,7 +12,7 @@ if [[ "$1" =~ [\.0-9]+$ ]]; then
 elif [[ "$1" =~ [\.0-9]+au$ ]]; then
     echo -e "\033[32m电场场强\033[0m \e[1;43m`printf "%20s" ${1//au}`\e[0m\t(原子单位)"
     [[ "$2" =~ [0-9]+ ]] && sca=$2 || sca=8
-    result=`echo "scale=$sca;sqrt(${1//au/} / 0.0533802681207856)/1"|bc`
+    result=`echo "scale=$sca;((${1//au/} / 0.0533802681207856)^2)/1"|bc`
     [[ $result =~ ^\..* ]] && result="0$result" || result="$result"
     [[ $result ]] &&  echo -e "\033[32m激光强度\033[0m \e[1;41m`printf "%20s" $result`\e[0m\t(\033[35m*e14\033[0m W/cm^2)"
 
